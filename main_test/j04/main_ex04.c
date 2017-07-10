76da9221a1d53d248228aa5cca8e1751fbb2c364
Adrien GILLMANN <agillman@e1r7p1.42.fr>
2017-07-10 15:17:16 +0200

    add main_test j04

---
+++ main_test/j04/main_ex04.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int		ft_fibonacci(int index);
+
+int		main(void)
+{
+	int a = 7;
+	int b = -1;
+	int c = 0;
+	int d = 1;
+
+	int res = ft_fibonacci(a);
+	printf("%d\n",res);
+	res=0;	
+	res = ft_fibonacci(b);
+	printf("%d\n",res);
+	res=0;
+	res = ft_fibonacci(c);
+	printf("%d\n",res);
+	res=0;
+	res = ft_fibonacci(d);
+	printf("%d\n",res);
+	res=0;
+}
---
+++ main_test/j04/main_ex05.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int		ft_sqrt(int index);
+
+int		main(void)
+{
+	int a = 9;
+	int b = -1;
+	int c = 0;
+	int d = 1;
+
+	int res = ft_sqrt(a);
+	printf("%d\n",res);
+	res=0;	
+	res = ft_sqrt(b);
+	printf("%d\n",res);
+	res=0;
+	res = ft_sqrt(c);
+	printf("%d\n",res);
+	res=0;
+	res = ft_sqrt(d);
+	printf("%d\n",res);
+	res=0;
+}
