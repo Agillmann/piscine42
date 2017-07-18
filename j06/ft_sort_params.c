a43d2855f69b71e1772fd37db40563364d0714bc
Adrien GILLMANN <agillman@e1r7p6.42.fr>
2017-07-18 17:05:50 +0200

    add ft_sort_params.c not finish

--- j06/ft_sort_params.c
+++ j06/ft_sort_params.c
@@ -6,11 +6,17 @@
 /*   By: agillman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/17 22:50:24 by agillman          #+#    #+#             */
-/*   Updated: 2017/07/17 23:04:23 by agillman         ###   ########.fr       */
+/*   Updated: 2017/07/18 16:30:35 by agillman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putchar(char c);
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
 void	ft_putstr(char *str)
 {
@@ -22,6 +28,39 @@ void	ft_putstr(char *str)
 		ft_putchar(str[i]);
 		i++;
 	}
+}
+
+int        ft_strlen(char *str)
+{
+	int cpt;
+
+	cpt = 0;
+	while (str[cpt])
+	{
+		cpt++;
+	}
+	return (cpt);
+}
+
+char    *ft_strcpy(char *dest, char *src)
+{
+	int i;
+	int len_dest;
+	int len_src;
+
+	i = 0;
+	len_dest = ft_strlen(dest);
+	len_src = ft_strlen(src);
+	if (len_dest < len_src)
+		return (src);
+	else
+		while (dest[i] != '\0')
+		{
+			dest[i] = src[i];
+			i++;
+		}
+	dest[i] = '\0';
+	return (dest);
 }
 
 int        ft_strcmp(char *s1, char *s2)
@@ -55,7 +94,29 @@ int        ft_strcmp(char *s1, char *s2)
 int		main(int argc, char **argv)
 {
 	int i;
-	
-	
-	
+	char tmp[argc];
 
+	i = 1;
+	while (i < argc - 1)
+	{
+		if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+		{
+			printf("argv[i] = %s argv[i + 1] = %s | ",argv[i],argv[i+1]);
+			
+			ft_strcpy(tmp, argv[i + 1]);
+			printf("tmp = %s | ", tmp);
+			ft_strcpy(argv[i + 1], argv[i]);
+			ft_strcpy(argv[i], tmp);
+			i = 1;
+		}
+		else
+			i++;
+	}
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putchar(' ');
+		i++;
+	}
+}
